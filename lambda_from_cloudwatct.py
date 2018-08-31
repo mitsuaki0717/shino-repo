d0c387f11694742ff70d6217774c7ac7a9c22d97
mitsuaki0717 <mitsuaki0717@gmail.com>
2018-08-31 11:55:35 +0900

    Update lambda_from_cloudwatct.py

--- lambda_from_cloudwatct.py
+++ lambda_from_cloudwatct.py
@@ -9,15 +9,14 @@ import slackweb
 
 def lambda_handler(event, context):
     client = boto3.client('logs')
-    #response = client.describe_log_groups(
-    #    logGroupNamePrefix='CloudTrail/DefaultLogGroup',
-    #    limit=2
-    #)
+
     response = client.get_log_events(
     logGroupName='CloudTrail/DefaultLogGroup',
     logStreamName='211832652529_CloudTrail_ap-northeast-1',
     limit=1
     )
-    return str(response["events"])
+
+    message_dist = json.loads(response["events"][0]["message"])
+#    return str(message_dist["userIdentity"]["sessionContext"]["attributes"])
     slack = slackweb.Slack(url="https://hooks.slack.com/services/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")
-    slack.notify(text="testです" + (response["events"])),  #通知内容
+    slack.notify(text=str(message_dist["userIdentity"]["sessionContext"]["attributes"])),  #通知内容
